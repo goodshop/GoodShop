e1c297485b12df34f693b259cc940c2739cd1376
Gibran Gonzalez <haibrayn@hotmail.com>
2014-04-01 17:13:03 -0600

    Added weird test page
    Signed-off-by: Gibran Gonzalez <haibrayn@hotmail.com>

---
+++ src/signups/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils.encoding import smart_unicode
+# Create your models here.
+class SignUp(models.Model):
+    for_you = models.BooleanField(default=True,
+                                  verbose_name="Does this make sense to you? If so, check this box.")
+    fname = models.CharField(max_length=120,
+                             null=True,
+                             blank=True,
+                             verbose_name="First Name"
+                             )
+    lname = models.CharField(max_length=120,
+                             null=True,
+                             blank=True,
+                             verbose_name="Last Name"
+                             )
+    email = models.EmailField()
+    timestamp = models.DateTimeField(auto_now_add=True,
+                                     auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False,
+                                   auto_now=True)
+
+    def __unicode__(self):
+        return smart_unicode(self.email)
